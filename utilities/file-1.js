fbc1fb66717fd107cf935ce04cc4d838309eaa3e
Abosede Awoniyi <awoniy.abosede@gmail.com>
2022-05-16 15:59:28 +0100

    different of importing updated

--- utilities/file-1.js
+++ utilities/file-1.js
@@ -1,5 +1,5 @@
 // find the largest number in an array
-const largeNumber = (arr) => {
+exports.largeNumber = (arr) => {
     let largest = 0;
     arr.forEach((x) => {
       if(x > largest){
@@ -11,12 +11,8 @@ const largeNumber = (arr) => {
   }
 
   // remove the 3rd item from array
-  const cut3 = (arr) => {
+  exports.cut3 = (arr) => {
     arr.splice(2, 1);
     return arr
   }
 
-  module.exports = {
-    largeNumber,
-    cut3, 
-  } 
