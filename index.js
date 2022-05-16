1c66566430890acc0da554a7796f7e867acc2107
Abosede Awoniyi <awoniy.abosede@gmail.com>
2022-05-16 16:10:58 +0100

    updating index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const file1 = require('./utilities/file-1')
 const { sum, concat } = require('./utilities/file-2')
 
-// File 1
+/*
+
+// Importing  File 1
+
+*/
 
 // find the largest number in an array
 console.log(file1.largeNumber([102,4,5]))
@@ -9,10 +13,15 @@ console.log(file1.largeNumber([102,4,5]))
 // remove the 3rd item from array
 console.log(file1.cut3([2,3,4]))
 
-// File 2
+
+/*
+
+// Importing File 2
+
+*/
 
 // add numbers in an array
 console.log(sum([2,50,67,80]))
 
-  // concatenate two arrays
+// concatenate two arrays
 console.log(concat([3,5,6],[2,4,56]))
