7823158073c63bf48649582f3e345ea0d0e49929
Abosede Awoniyi <awoniy.abosede@gmail.com>
2022-05-16 15:13:27 +0100

    Uploading all files used

---
+++ index.js
@@ -0,0 +1,18 @@
+const file1 = require('./utilities/file-1')
+const file2 = require('./utilities/file-2')
+
+// File 1
+
+// find the largest number in an array
+console.log(file1.largeNumber([102,4,5]))
+
+// remove the 3rd item from array
+console.log(file1.cut3([2,3,4]))
+
+// File 2
+
+// add numbers in an array
+console.log(file2.sum([2,50,67,80]))
+
+  // concatenate two arrays
+console.log(file2.concat([3,5,6],[2,4,56]))
